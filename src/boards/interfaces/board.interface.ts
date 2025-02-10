6877d03407c834aa5cf31517f738bd0b18d6bc10
0x-alx <alx.charbo@gmail.com>
2025-02-10 14:01:43 +0100

    Refactor project structure and update data model

--- src/boards/interfaces/board.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface Board {
-  // Define your board structure here based on data.json
-  [key: string]: any; 
-} 
--- src/interfaces/data.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface Data {
-  // Define your data structure here based on data.json
-  [key: string]: any; 
-} 
