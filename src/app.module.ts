95d6a31281a984b5ee31fde445b88668bc19d1dc
0x-alx <alx.charbo@gmail.com>
2025-02-12 11:11:06 +0100

    Add NestJS Config module for environment configuration and update Firebase service to use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { BoardsModule } from './boards/boards.module';
 import { ColumnsModule } from './columns/columns.module';
 import { LoggerModule } from './logger/logger.module';
 import { SubtasksModule } from './subtasks/subtasks.module';
 import { TasksModule } from './tasks/tasks.module';
-
 @Module({
   imports: [
     LoggerModule,
     BoardsModule,
     ColumnsModule,
     TasksModule,
-    SubtasksModule
+    SubtasksModule,
+    ConfigModule.forRoot(),
   ],
   controllers: [],
   providers: [],
